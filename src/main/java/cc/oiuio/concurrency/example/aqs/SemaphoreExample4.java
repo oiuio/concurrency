b3c9c5c38a4263cb977c7536b0ca4c97206839a2
oiuio <oiuio.cc@qq.com>
2018-09-25 18:13:45 +0800

    aqs

---
+++ src/main/java/cc/oiuio/concurrency/example/aqs/SemaphoreExample4.java
@@ -0,0 +1,41 @@
+package cc.oiuio.concurrency.example.aqs;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+public class SemaphoreExample4 {
+
+	private final static int threadCount = 200;
+
+	public static void main(String[] args) throws InterruptedException {
+
+		ExecutorService exec = Executors.newCachedThreadPool();
+
+		final Semaphore semaphore = new Semaphore(3);
+
+		for (int i = 0; i < threadCount; i++) {
+			final int threadNum = i;
+			exec.execute(() -> {
+				try {
+					if (semaphore.tryAcquire(3000,TimeUnit.MILLISECONDS)){
+						test(threadNum);
+						semaphore.release();
+					}
+				} catch (InterruptedException e) {
+					log.error("{}", e);
+				}
+			});
+		}
+		exec.shutdown();
+	}
+
+	private static void test(int threadNum) throws InterruptedException {
+		log.info("{}", threadNum);
+		Thread.sleep(1000);
+	}
+}
---
+++ src/main/java/cc/oiuio/concurrency/example/concurrent/ConcurrentSkipListMapExample.java
@@ -0,0 +1,49 @@
+package cc.oiuio.concurrency.example.concurrent;
+
+import cc.oiuio.concurrency.annoations.ThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Map;
+import java.util.concurrent.*;
+
+@Slf4j
+@ThreadSafe
+public class ConcurrentSkipListMapExample {
+
+	//请求总数
+	public static int clientTotal = 5000;
+
+	//并发线程数
+	public static int threadTotal = 200;
+
+	private static Map<Integer, Integer> map = new ConcurrentSkipListMap<>();
+
+	public static void main(String[] args) throws InterruptedException {
+		//线程池
+		ExecutorService executorService = Executors.newCachedThreadPool();
+		//信号量
+		final Semaphore semaphore = new Semaphore(threadTotal);
+		//计数器闭锁
+		final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+		for (int i = 0; i < clientTotal; i++) {
+			final int count = i;
+			executorService.execute(() -> {
+				try {
+					semaphore.acquire();
+					update(count);
+					semaphore.release();
+				} catch (Exception e) {
+					log.error("exception", e);
+				}
+				countDownLatch.countDown();
+			});
+		}
+		countDownLatch.await();
+		executorService.shutdown();
+		log.info("size:{}", map.size());
+	}
+
+	private static void update(int i) {
+		map.put(i, i);
+	}
+}
